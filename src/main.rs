6834cadbc66f0ff9fb6d4b7e9f94bd03dc0a4f42
Ian O'Rourke <cessna.ian@gmail.com>
2021-05-30 21:35:10 -0500

    Provide a CPU step

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 mod memory;
 mod cpu;
 
-use memory::read_write_memory::ReadWriteMemory;
+//use memory::read_write_memory::ReadWriteMemory;
 
 fn main()
 {
     println!("Initializing CPU");
-    let scpu = cpu::processor::SolariumCPU::new();
+    let mut cpu = cpu::processor::SolariumCPU::new();
+    cpu.step();
 }
-
