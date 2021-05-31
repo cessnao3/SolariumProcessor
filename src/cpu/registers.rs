17f3b376ce10931934cec963fa58d3cad2c9abfa
Ian O'Rourke <cessna.ian@gmail.com>
2021-05-30 20:17:48 -0500

    Update register and add tests

--- src/cpu/registers.rs
+++ src/cpu/registers.rs
@@ -1,53 +1,47 @@
 use crate::memory::MemoryWord;
 
+/// Defines the number of registers available in the processor
 const NUM_REGISTERS: usize = 16;
 
+#[derive(Clone)]
+/// Defines the enumeration for available registers
 pub enum Register
 {
     ProgramCounter,
     StackPointer,
     StatusFlag,
     Zero,
-    GP4,
-    GP5,
-    GP6,
-    GP7,
-    GP8,
-    GP9,
-    GP10,
-    GP11,
-    GP12,
-    GP13,
-    GP14,
-    GP15
+    GP(usize)
 }
 
 impl Register
 {
-    pub fn to_index(&self) -> usize
+    /// Converts the enumeration types to an index for a registry array
+    fn to_index(&self) -> usize
     {
-        return match self
+        // Determine the index
+        let ind: usize = match self
         {
             Register::ProgramCounter => 0,
             Register::StackPointer => 1,
             Register::StatusFlag => 2,
             Register::Zero => 3,
-            Register::GP4 => 4,
-            Register::GP5 => 5,
-            Register::GP6 => 6,
-            Register::GP7 => 7,
-            Register::GP8 => 8,
-            Register::GP9 => 9,
-            Register::GP10 => 10,
-            Register::GP11 => 11,
-            Register::GP12 => 12,
-            Register::GP13 => 13,
-            Register::GP14 => 14,
-            Register::GP15 => 15
+            Register::GP(ind) => *ind
         };
+
+        // Check for the index values
+        if ind < NUM_REGISTERS
+        {
+            return ind;
+        }
+        else
+        {
+            panic!("Register index is greater than available processor registers");
+        }
     }
 }
 
+/// Defines a register manager to maintain register values
 pub struct RegisterManager
 {
     registers: [MemoryWord; NUM_REGISTERS]
@@ -55,6 +49,7 @@ pub struct RegisterManager
 
 impl RegisterManager
 {
+    /// Creates a new register manager
     pub fn new() -> RegisterManager
     {
         return RegisterManager
@@ -63,6 +58,7 @@ impl RegisterManager
         };
     }
 
+    /// Resets all registers to a known, zero, state
     pub fn reset(&mut self)
     {
         for v in self.registers.iter_mut()
@@ -71,17 +67,99 @@ impl RegisterManager
         }
     }
 
-    pub fn get(&self, register: Register) -> MemoryWord
+    /// Gets the selected register value
+    pub fn get(&self, register: &Register) -> MemoryWord
     {
         return self.registers[register.to_index()];
     }
 
-    pub fn set(&mut self, register: Register, value: MemoryWord)
+    /// Sets the selecteed register value
+    pub fn set(&mut self, register: &Register, value: MemoryWord)
     {
-        self.registers[register.to_index()] = match register
-        {
-            Register::Zero => 0,
-            _ => value
+        let reg_ind = register.to_index();
+        self.registers[reg_ind] = if reg_ind == Register::Zero.to_index() { 0 } else { value };
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    /// Provide the register vector for available registers
+    fn get_registers() -> Vec<Register>
+    {
+        // Define the list of named registers
+        let mut registers: Vec<Register> = vec!{
+            Register::Zero,
+            Register::ProgramCounter,
+            Register::StackPointer,
+            Register::StatusFlag
         };
+
+        // Add the register indices
+        for i in 0..NUM_REGISTERS
+        {
+            registers.push(Register::GP(i));
+        }
+
+        // Return the register vector
+        return registers;
+    }
+
+    #[test]
+    /// Tests the general get/set register values
+    fn test_set_get_register()
+    {
+        // Create a register manager to test register values
+        let mut register_manager = RegisterManager::new();
+
+        // Determine the register index
+        let zero_ind = Register::Zero.to_index();
+
+        // Iterate over available registers
+        for register in get_registers().iter()
+        {
+            // Determine values ot iterate over
+            for v in MemoryWord::MIN..MemoryWord::MAX
+            {
+                // Define the expected value, ensuring that the zero register is always 0
+                let expected_value: MemoryWord = if register.to_index() == zero_ind
+                {
+                    0
+                }
+                else
+                {
+                    v
+                };
+
+                // Set the register manager, and then ensure that the output result matches
+                register_manager.set(&register, v);
+                assert_eq!(register_manager.get(&register), expected_value);
+            }
+        }
+    }
+
+    #[test]
+    /// Test that each register is stored in its own storage location
+    fn test_individual_values()
+    {
+        // Define a register manager
+        let mut register_manager = RegisterManager::new();
+
+        // Add a value to each register
+        for i in 0..NUM_REGISTERS
+        {
+            register_manager.set(&Register::GP(i), i as MemoryWord);
+        }
+
+        // Ensure that we can get the resulting value out
+        for i in 0..NUM_REGISTERS
+        {
+            let val = register_manager.get(&Register::GP(i));
+
+            let expected = if i == Register::Zero.to_index() { 0 } else { i as MemoryWord };
+
+            assert_eq!(val, expected);
+        }
     }
 }
