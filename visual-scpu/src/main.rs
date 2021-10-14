709bd0a1bee77fd014747441010ad5314db80f16
Ian O'Rourke <cessna.ian@gmail.com>
2021-10-14 13:06:10 -0500

    Update window values

--- visual-scpu/src/main.rs
+++ visual-scpu/src/main.rs
@@ -5,8 +5,11 @@ fn main()
 {
     // Define the window values
     let app = app::App::default();
-    let mut wind = Window::new(100, 100, 400, 300, "Hello from rust");
-    let mut frame = Frame::new(0, 0, 400, 200, "");
+    //let mut wind = Window::new(100, 100, 400, 300, "Hello from rust");
+    let mut wind = Window::default()
+        .with_size(700, 600)
+        .with_label("VisualSCPU");
+    let mut frame = Frame::new(0, 0, 400, 200, "").set_color(Color::blu);
     let mut but = Button::new(160, 210, 80, 40, "Click me!");
     wind.end();
     wind.show();
