53c9a438e3e979e053b67096beb8c2e179dd3d11
Ian O'Rourke <cessna.ian@gmail.com>
2021-10-15 13:25:18 -0500

    Updates to VisualSCPU GUI

--- visual-scpu/src/main.rs
+++ visual-scpu/src/main.rs
@@ -1,14 +1,34 @@
 use fltk::prelude::*;
-use fltk::{app::*, button::*, dialog::*, enums::*, window::*, text::*, group::*, frame::*};
+use fltk::{app::*, button::*, dialog::*, enums::*, window::*, text::*, group::*, frame::*,};
+use fltk::text::TextEditor;
 
 use libscpu::cpu::SolariumCPU;
 use libscpu::memory::ReadWriteSegment;
+
+use std::thread;
+use std::time;
+
+use std::sync::mpsc;
 
 #[derive(Clone, Copy)]
 enum Message
 {
     Step,
-    UpdateRegisters
+    UpdateRegisters,
+    Start,
+    Stop,
+    Reset,
+    Tick,
+    Assemble,
+    Log
+}
+
+#[derive(Clone, Copy)]
+enum ThreadMessage
+{
+    Start,
+    Stop,
+    Exit
 }
 
 fn main()
@@ -20,42 +40,109 @@ fn main()
     // Define the window values
     let app = App::default();
 
-    let (s, r) = channel::<Message>();
+    let (sender, receiver) = channel::<Message>();
 
     //let mut wind = Window::new(100, 100, 400, 300, "Hello from rust");
     let mut wind = Window::default()
         .with_size(1100, 600)
         .with_label("VisualSCPU");
 
+    let mut main_group = Flex::default_fill().row();
+    let mut assembly_editor;
+
+    let mut editor_group = Flex::default_fill().column();
+    {
+        let mut assembly_label = Frame::default().with_label("Assembly Editor");
+        assembly_editor = TextEditor::default();
+        assembly_editor.set_buffer(TextBuffer::default());
+
+        editor_group.set_size(&mut assembly_label, 50);
+
+        let mut assemble_button = Button::default().with_label("Assemble");
+        assemble_button.emit(sender, Message::Assemble);
+
+        editor_group.set_size(&mut assemble_button, 50);
+    }
+    editor_group.end();
+
+    //column_group.set_size(&mut text_editor, 100);
+
     let mut register_group = Flex::default_fill().column();
+    let register_labels;
+    {
+        // Add a simple step initiation button
+        let mut button_group = Flex::default_fill().row();
 
-    // Add a simple step initiation button
-    let mut button_group = Flex::default().row();
-    let mut step_button = Button::default().with_label("STEP");
-    step_button.emit(s, Message::Step);
-    button_group.set_size(&mut step_button, 100);
-    button_group.end();
-    register_group.set_size(&mut button_group, 50);
+        let mut step_button = Button::default().with_label("STEP");
+        step_button.emit(sender, Message::Step);
 
-    // Define the registers and perform the initial update values
-    let register_labels = define_registers(&mut register_group);
+        let mut start_button = Button::default().with_label("START");
+        start_button.emit(sender, Message::Start);
 
+        let mut stop_button = Button::default().with_label("STOP");
+        stop_button.emit(sender, Message::Stop);
+
+        let mut reset_button = Button::default().with_label("RESET");
+        reset_button.emit(sender, Message::Reset);
+
+        button_group.set_size(&mut step_button, 70);
+        button_group.set_size(&mut start_button, 70);
+        button_group.set_size(&mut stop_button, 70);
+        button_group.set_size(&mut reset_button, 70);
+
+        button_group.end();
+        register_group.set_size(&mut button_group, 50);
+
+        // Define the registers and perform the initial update values
+        register_labels = define_registers(&mut register_group);
+    }
     // End the register group
     register_group.end();
+
+    main_group.set_size(&mut register_group, 350);
+
+    main_group.end();
 
     // Finish Window Setup
     wind.end();
 
     // Init Parameters
-    s.send(Message::UpdateRegisters);
+    sender.send(Message::UpdateRegisters);
+
+    let (thread_tx, thread_rx) = mpsc::channel();
+
+    //thread_tx.send(ThreadMessage::Start);
+
+    // Setup the start/stop timer
+    thread::spawn(move || {
+        let mut step_cpu = false;
+        loop {
+            match thread_rx.try_recv()
+            {
+                Ok(v) => match v
+                {
+                    ThreadMessage::Exit => break,
+                    ThreadMessage::Start => step_cpu = true,
+                    ThreadMessage::Stop => step_cpu = false
+                },
+                Err(mpsc::TryRecvError::Disconnected) => break,
+                Err(mpsc::TryRecvError::Empty) => ()
+            };
+
+            if step_cpu
+            {
+                thread::sleep(time::Duration::from_millis(10));
+                sender.send(Message::Tick);
+            }
+    }});
 
     // Show the window and run the application
     wind.show();
     while app.wait()
     {
-        if let Some(msg) = r.recv() {
+        if let Some(msg) = receiver.recv() {
             match msg {
-                Message::Step =>
+                Message::Step | Message::Tick =>
                 {
                     match cpu.step()
                     {
@@ -70,7 +157,7 @@ fn main()
                         }
                     }
 
-                    s.send(Message::UpdateRegisters);
+                    sender.send(Message::UpdateRegisters);
                 },
                 Message::UpdateRegisters =>
                 {
@@ -78,7 +165,22 @@ fn main()
                     {
                         register_labels[i].buffer().unwrap().set_text(&format!("{0:}", cpu.get_register_value(i)));
                     }
-                }
+                },
+                Message::Start =>
+                {
+                    thread_tx.send(ThreadMessage::Start);
+                },
+                Message::Stop =>
+                {
+                    thread_tx.send(ThreadMessage::Stop);
+                },
+                Message::Reset =>
+                {
+                    thread_tx.send(ThreadMessage::Stop);
+                    cpu.reset();
+                },
+                Message::Assemble => (),
+                Message::Log => ()
             }
         }
     }
@@ -129,7 +231,7 @@ fn setup_register(parent: &mut Flex, index: usize) -> TextDisplay
     let buffer = TextBuffer::default();
     text_display.set_buffer(buffer);
 
-    parent.set_size(&mut label, 40);
+    parent.set_size(&mut label, 50);
     parent.set_size(&mut text_display, 100);
 
     return text_display;
