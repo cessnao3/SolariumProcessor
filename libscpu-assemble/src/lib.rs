d73103947dd2d2eff69d269ae9d5b4b00719b4c8
Ian O'Rourke <cessna.ian@gmail.com>
2021-10-16 23:17:39 -0500

    Add basic commands to assembler

--- libscpu-assemble/src/lib.rs
+++ libscpu-assemble/src/lib.rs
@@ -6,92 +6,140 @@ use std::collections::HashMap;
 
 use regex::Regex;
 
-fn cleanup_lines(lines: Vec<String>) -> Vec<String>
-{
-    let mut new_lines = Vec::new();
+const MAX_ADDRESSABLE_VALUE: usize = (2usize).pow(16);
 
-    for line in lines.iter()
-    {
-        let result_string: &str = match line.find("//")
-        {
-            Some(len) => &line[..len],
-            None => line
-        }.trim();
-
-        if result_string.len() > 0
-        {
-            new_lines.push(result_string.to_string());
-        }
-    }
-
-    return new_lines;
-}
 
 pub fn assemble(lines: Vec<String>) -> Result<Vec<u16>, String>
 {
     let line_regex = Regex::new(r"^(?P<instruction>[\w]+)(?P<rest>\s+\-?[\w\d]+(,\s*\-?[\w\d]+)*)*$").unwrap();
+    let command_regex = Regex::new(r"^\.(?P<command>[\w]+)\s+(?P<args>[\w\d\s]*)$").unwrap();
     let args_split_regex = Regex::new(r",\s*").unwrap();
 
-    let mut data_values = HashMap::<u16, u16>::new();
-    let mut data_offset = 0u16;
+    let mut data_values = HashMap::<usize, u16>::new();
+    let mut data_offset = 0usize;
 
     let instruction_map = get_instruction_map();
 
-    for (i, l) in cleanup_lines(lines).iter().enumerate()
+    for (i, l_in) in lines.iter().enumerate()
     {
+        // Cleanup the resulting string value
+        let l: &str = match l_in.find("//")
+        {
+            Some(len) => &l_in[..len],
+            None => l_in
+        }.trim();
+
         // Check for line validity
         if l.is_empty()
         {
             continue;
         }
-        else if !line_regex.is_match(l)
+        else if command_regex.is_match(l)
+        {
+            // Extract the command captures
+            let caps = match command_regex.captures(l)
+            {
+                Some(v) => v,
+                None => return Err(format!("line {0:} no command captures found for \"{1:}\"", i, l))
+            };
+
+            // Extract the resulting instruction values
+            let cmd = caps.name("command").unwrap().as_str().to_ascii_lowercase();
+            let args = match caps.name("args")
+            {
+                Some(v) => args_split_regex
+                    .split(&v.as_str().trim().to_ascii_lowercase())
+                    .map(|v| v.to_string())
+                    .collect(),
+                None => Vec::new()
+            };
+
+            // Check argument value
+            if cmd == "oper"
+            {
+                if args.len() != 1
+                {
+                    return Err(format!("line {0:} command {1:} only takes 1 argument", i, cmd));
+                }
+
+                let new_offset = match args[0].parse::<usize>()
+                {
+                    Ok(v) => v,
+                    Err(_) => return Err(format!("line {0:} command {1:} unable to parse {2:} as address", i, cmd, args[0]))
+                };
+
+                if new_offset < data_offset
+                {
+                    return Err(format!("line {0:} command {1:} new offset {2:} must be greater or equal to current offset {3:}", i, cmd, new_offset, data_offset));
+                }
+                else
+                {
+                    data_offset = new_offset;
+                }
+            }
+            else
+            {
+                return Err(format!("line {0:} invalid command \"{1:}\" found", i, cmd));
+            }
+        }
+        else if line_regex.is_match(l)
+        {
+            // Extract the captures
+            let caps = match line_regex.captures(l)
+            {
+                Some(v) => v,
+                None => return Err(format!("line {0:} no captures found for \"{1:}\"", i, l))
+            };
+
+            // Extract the instruction string and the argument string
+            let inst = caps.name("instruction").unwrap().as_str().to_ascii_lowercase();
+            let args = match caps.name("rest")
+            {
+                Some(v) => args_split_regex
+                    .split(&v.as_str().trim().to_ascii_lowercase())
+                    .map(|v| v.to_string())
+                    .collect(),
+                None => Vec::new()
+            };
+
+            // Extract the expected output value
+            let inst_val = match instruction_map.get(&inst)
+            {
+                Some(v) => v,
+                None => return Err(format!("line {0:} no instruction {1:} found", i, inst))
+            };
+
+            // Attempt to create the resulting data
+            let inst_data = match inst_val.to_instruction_data(&args)
+            {
+                Ok(v) => v,
+                Err(e) => return Err(format!("line {0:} {1:} -> {2:}", i, inst, e))
+            };
+
+            // Add the resulting instruction value
+            if data_values.contains_key(&data_offset)
+            {
+                return Err(format!("line {0:} offset {1:} already filled", i, data_offset))
+            }
+
+            // Check the current address value
+            if data_offset < MAX_ADDRESSABLE_VALUE
+            {
+                // Add the resulting data value
+                data_values.insert(
+                    data_offset,
+                    inst_data.combine());
+                data_offset += 1;
+            }
+            else
+            {
+                return Err(format!("line {0:} offset {1:} is greater than the size {2:} allows", i, data_offset, MAX_ADDRESSABLE_VALUE));
+            }
+        }
+        else
         {
             return Err(format!("line {0:} \"{1:}\" does not match expected syntax", i, l));
         }
-
-        // Extract the captures
-        let caps = match line_regex.captures(l)
-        {
-            Some(v) => v,
-            None => return Err(format!("line {0:} no captures found for \"{1:}\"", i, l))
-        };
-
-        // Extract the instruction string and the argument string
-        let inst = caps.name("instruction").unwrap().as_str().to_lowercase();
-        let args = match caps.name("rest")
-        {
-            Some(v) => args_split_regex
-                .split(&v.as_str().trim().to_lowercase())
-                .map(|v| v.to_string())
-                .collect(),
-            None => Vec::new()
-        };
-
-        // Extract the expected output value
-        let inst_val = match instruction_map.get(&inst)
-        {
-            Some(v) => v,
-            None => return Err(format!("line {0:} no instruction {1:} found", i, inst))
-        };
-
-        // Attempt to create the resulting data
-        let inst_data = match inst_val.to_instruction_data(&args)
-        {
-            Ok(v) => v,
-            Err(e) => return Err(format!("line {0:} {1:} -> {2:}", i, inst, e))
-        };
-
-        // Add the resulting instruction value
-        if data_values.contains_key(&data_offset)
-        {
-            return Err(format!("line {0:} offset {1:} already filled", i, data_offset))
-        }
-
-
-        data_values.insert(
-            data_offset,
-            inst_data.combine());
-        data_offset += 1;
     }
 
     let max_index = match data_values.keys().max()
