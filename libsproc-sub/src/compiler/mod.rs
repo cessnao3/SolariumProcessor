66475a35000e6363a995a7a8707f4c7792025539
Ian O'Rourke <cessna.ian@gmail.com>
2021-12-02 20:10:44 -0600

    Remove printout of tokens and resulting assembly

--- libsproc-sub/src/compiler/mod.rs
+++ libsproc-sub/src/compiler/mod.rs
@@ -9,23 +9,12 @@ pub fn compile(text: &str) -> Result<Vec<String>, String>
         Err(e) => return Err(format!("Tokenizer Error: {0:}", e))
     };
 
-    println!("Tokens:");
-    for t in &tokens
-    {
-        println!("  {0:}", t.to_string());
-    }
-
     let assembly_result;
 
-    println!("Compiling...");
     match lexer(tokens)
     {
         Ok(v) =>
         {
-            for s in v.iter()
-            {
-                println!("  {0:}", s);
-            }
             assembly_result = Ok(v);
         },
         Err(e) => {
