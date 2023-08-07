f20825f9df70b88f8a3514b67c95bc58d5d4e692
Ian O'Rourke <cessna.ian@gmail.com>
2023-08-07 18:30:31 -0500

    Start work on GTK variant

---
+++ visual-sproc-gtk/src/main.rs
@@ -0,0 +1,43 @@
+use gtk::prelude::*;
+use adw::prelude::*;
+use gtk::{glib, Application, ApplicationWindow, Button};
+
+const APP_ID: &str = "com.orourke.Solarium.VisualSProc";
+
+fn main() -> glib::ExitCode {
+    // Create a new application
+    let app = adw::Application::builder().application_id(APP_ID).build();
+
+    // Connect to "activate" signal of `app`
+    app.connect_activate(build_ui);
+
+    // Run the application
+    app.run()
+}
+
+fn build_ui(app: &adw::Application) {
+    // Create a button with label and margins
+    let button = Button::builder()
+        .label("Press me!")
+        .margin_top(12)
+        .margin_bottom(12)
+        .margin_start(12)
+        .margin_end(12)
+        .build();
+
+    // Connect to "clicked" signal of `button`
+    button.connect_clicked(|button| {
+        // Set the label to "Hello World!" after the button has been clicked on
+        button.set_label("Hello World!");
+    });
+
+    // Create a window and set the title
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("My GTK App")
+        .child(&button)
+        .build();
+
+    // Present window
+    window.present();
+}
