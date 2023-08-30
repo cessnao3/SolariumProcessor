f951cc3533cd84b071aafbf010395864292ff4da
Ian O'Rourke <cessna.ian@gmail.com>
2023-08-30 06:33:24 -0500

    Update code window

--- visual-sproc-gtk/src/main.rs
+++ visual-sproc-gtk/src/main.rs
@@ -1,4 +1,4 @@
-use gtk::glib::clone;
+//use gtk::glib::clone;
 use gtk::{prelude::*, StackSwitcher, Stack};
 use gtk::{glib, Application, ApplicationWindow};
 use gtk::{TextView, TextBuffer, Frame, ScrolledWindow, Button, Box};
@@ -26,22 +26,6 @@ fn build_ui(app: &Application) {
         .margin_end(12)
         .build();
 
-    let buffer_code = TextBuffer::builder()
-        .enable_undo(true)
-        .text("TESTING!")
-        .build();
-
-    let text_code = TextView::builder()
-        .buffer(&buffer_code)
-        .width_request(300)
-        .height_request(400)
-        .has_tooltip(true)
-        .hexpand(true)
-        .vexpand(true)
-        .tooltip_text("Text input for assembly code")
-        .monospace(true)
-        .build();
-
     let buffer_log = TextBuffer::builder().build();
 
     let text_log = TextView::builder()
@@ -60,21 +44,73 @@ fn build_ui(app: &Application) {
         .child(&text_log)
         .build();
 
-    let text_code_scroll = ScrolledWindow::builder()
-        .child(&text_code)
-        .build();
-
-    let text_code_frame = Frame::builder()
-        .label("Assembly Code Editor")
-        .child(&text_code_scroll)
-        .build();
-
     let code_stack = Stack::builder().build();
-    let p1 = code_stack.add_child(&text_code_frame);
-    p1.set_title("Assembly");
-    let p2 = code_stack.add_child(&Button::builder().label("TESTING!").build());
-    p2.set_title("SPC");
-    let code_switcher = StackSwitcher::builder().stack(&code_stack).build();
+
+    let buffer_assembly_code = TextBuffer::builder()
+        .enable_undo(true)
+        .text("; ASM Code")
+        .build();
+    {
+        let text_code = TextView::builder()
+            .buffer(&buffer_assembly_code)
+            .width_request(300)
+            .height_request(400)
+            .has_tooltip(true)
+            .hexpand(true)
+            .vexpand(true)
+            .tooltip_text("Text input for assembly code")
+            .monospace(true)
+            .build();
+        let text_code_scroll = ScrolledWindow::builder()
+            .child(&text_code)
+            .build();
+        let text_code_frame = Frame::builder()
+            .label("Assembly Code Editor")
+            .child(&text_code_scroll)
+            .build();
+
+        let code_box = Box::builder().orientation(gtk::Orientation::Vertical).spacing(4).build();
+        let btn_build = Button::builder().label("Assemble").build();
+
+        code_box.append(&text_code_frame);
+        code_box.append(&btn_build);
+
+        let page_asm = code_stack.add_child(&code_box);
+        page_asm.set_title("ASM");
+    }
+
+    let buffer_spc_code = TextBuffer::builder()
+        .enable_undo(true)
+        .text("// SPC Code")
+        .build();
+    {
+        let text_code = TextView::builder()
+            .buffer(&buffer_spc_code)
+            .width_request(300)
+            .height_request(400)
+            .has_tooltip(true)
+            .hexpand(true)
+            .vexpand(true)
+            .tooltip_text("Text input for SPC code")
+            .monospace(true)
+            .build();
+        let text_code_scroll = ScrolledWindow::builder()
+            .child(&text_code)
+            .build();
+        let text_code_frame = Frame::builder()
+            .label("SPC Code Editor")
+            .child(&text_code_scroll)
+            .build();
+
+        let code_box = Box::builder().orientation(gtk::Orientation::Vertical).spacing(4).build();
+        let btn_build = Button::builder().label("Compile").build();
+
+        code_box.append(&text_code_frame);
+        code_box.append(&btn_build);
+
+        let page_spc = code_stack.add_child(&code_box);
+        page_spc.set_title("SPC");
+    }
 
     // Connect to "clicked" signal of `button`
     button.connect_clicked(|button| {
@@ -83,23 +119,22 @@ fn build_ui(app: &Application) {
     });
 
     let columns = Box::builder().vexpand(true).hexpand(true).orientation(gtk::Orientation::Horizontal).spacing(4).margin_top(4).margin_bottom(4).margin_start(4).margin_end(4).build();
-    let column_1 = Box::builder().vexpand(true).hexpand(true).orientation(gtk::Orientation::Vertical).spacing(4).build();
+    let column_code = Box::builder().vexpand(true).hexpand(true).orientation(gtk::Orientation::Vertical).spacing(4).build();
     let column_2 = Box::builder().vexpand(true).hexpand(true).orientation(gtk::Orientation::Vertical).spacing(4).build();
 
     let btn1 = Button::builder().label("TESTING 1!").build();
     let btn2 = Button::builder().label("TESTING!").build();
 
-    btn1.connect_clicked(clone!(@weak buffer_code => move |b| b.set_label(&format!("B1: {}", buffer_code.text(&buffer_code.start_iter(), &buffer_code.end_iter(), false).as_str()))));
-    btn2.connect_clicked(clone!(@weak buffer_code => move |b| b.set_label(&format!("B2: {}", buffer_code.text(&buffer_code.start_iter(), &buffer_code.end_iter(), false).as_str()))));
+    //btn1.connect_clicked(clone!(@weak buffer_code => move |b| b.set_label(&format!("B1: {}", buffer_code.text(&buffer_code.start_iter(), &buffer_code.end_iter(), false).as_str()))));
+    //btn2.connect_clicked(clone!(@weak buffer_code => move |b| b.set_label(&format!("B2: {}", buffer_code.text(&buffer_code.start_iter(), &buffer_code.end_iter(), false).as_str()))));
 
-    column_1.append(&code_switcher);
-    column_1.append(&code_stack);
-    column_1.append(&button);
+    column_code.append(&StackSwitcher::builder().stack(&code_stack).build());
+    column_code.append(&code_stack);
     column_2.append(&btn1);
     column_2.append(&btn2);
     column_2.append(&text_log_frame);
 
-    columns.append(&column_1);
+    columns.append(&column_code);
     columns.append(&column_2);
 
     // Create a window and set the title
